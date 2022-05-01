f983f72dac955b5b3d904cf251f85ff878a4541a
satomacoto <satomacoto@gmail.com>
2022-05-01 16:38:14 +0900

    Rename module

---
+++ ipat/baken_syurui.py
@@ -0,0 +1,31 @@
+class BakenSyurui:
+    TAN = 1
+    FUKU = 2
+    WAKU = 3
+    UMAREN = 4
+    WIDE = 5
+    UMATAN = 6
+    SANFUKU = 7
+    SANTAN = 8
+
+    ALL = [
+        TAN,
+        FUKU,
+        WAKU,
+        UMAREN,
+        WIDE,
+        UMATAN,
+        SANFUKU,
+        SANTAN,
+    ]
+
+    NAMES = {
+        1: "TAN",
+        2: "FUKU",
+        3: "WAKU",
+        4: "UMAREN",
+        5: "WIDE",
+        6: "UMATAN",
+        7: "SANFUKU",
+        8: "SANTAN",
+    }
